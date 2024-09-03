a59cde287728ebf680d2e0d73efb839464052fd2
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-09-03 14:14:14 -0500

    make the card dynamic

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -14,7 +14,7 @@ import {
 const Navbar = () => {
   return (
     <section className=" bg-primary dark:bg-slate-700">
-      <nav className=" max-w-screen-2xl mx-auto text-white py-2 px-8 flex justify-between">
+      <nav className="  text-white py-2 px-8 flex justify-between">
         <Link href="/">
           <Image src={logo} alt="logo" width={40} />
         </Link>
