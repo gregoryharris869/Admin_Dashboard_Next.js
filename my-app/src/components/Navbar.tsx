dad5200dfba48a9350cd76ff95564ea301bc5257
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-09-02 23:01:29 -0500

    Sidebar

---
+++ my-app/src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../img/logo.png";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const Navbar = () => {
+  return (
+    <section className="w-full bg-primary dark:bg-slate-700">
+      <nav className=" max-w-screen-2xl mx-auto text-white py-2 px-8 flex justify-between">
+        <Link href="/">
+          <Image src={logo} alt="logo" width={40} />
+        </Link>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger className="focus:outline-none">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback className="text-black">GH</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href="/profile">Profile</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/auth">Logout</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </nav>
+    </section>
+  );
+};
+
+export default Navbar;
